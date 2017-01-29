0629db82e7f1ad6bba1a7e6c0e8d6eb3aac71bd9
jpbastida <jpbastida@hotmail.com>
2017-01-29 14:19:48 +0200

    Final

--- Control1/Program.cs
+++ Control1/Program.cs
@@ -10,6 +10,16 @@ namespace Control1
     {
         static void Main(string[] args)
         {
+            List<int> l1 = new List<int>()
+            {
+                1,2,3,4,5
+            };
+            List<int> l2 = new List<int>()
+            {
+                5,4,3,2,1
+            };
+
+
             // Task 1
             //Z1_1 b1 = new Z1_1();
             //b1.BooksSelection();
@@ -27,17 +37,18 @@ namespace Control1
             //b4.HowMany();
 
             // Task 5
-            int[] array = { 1, 2, 3, 4, 5 };
+            //int[] array = { 1, 2, 3, 4, 5 };
 
-            Z1_5 b5 = new Z1_5();
-            b5.Counter(array);
-            b5.Maximum(array);
-            b5.AverageValue(array);
+            //Z1_5 b5 = new Z1_5();
+            //b5.Counter(array);
+            //b5.Maximum(array);
+            //b5.AverageValue(array);
 
-            // Task 7
-
-
-
+            // Task 6
+            Z1_6 b6 = new Z1_6();
+            //b6.AllPairs(l1, l2);
+            //b6.NonEqualNumbersPairs(l1, l2);
+            b6.NonEqualPairs(l1, l2);
         }
     }
 }
--- Control1/Z1_6.cs
+++ Control1/Z1_6.cs
@@ -8,13 +8,35 @@ namespace Control1
 {
     class Z1_6
     {
-        public static void AllPairs(IEnumerable<int> list1, IEnumerable<int> list2)
+        public void Pairs(IEnumerable<int> list1, IEnumerable<int> list2)
         {
             var result = list1.Join(list2, l1 => true, l2 => true, (l1, l2) => new { val1 = l1, val2 = l2 });
+                
             foreach (var item in result)
             {
-                Console.Write(item.val1 + " ");
-                Console.WriteLine(item.val2);
+                Console.WriteLine($"{item.val1} {item.val2}");
+            }
+        }
+
+        public void NonEqualNumbersPairs(IEnumerable<int> list1, IEnumerable<int> list2)
+        {
+            var result = list1.Join(list2, l1 => true, l2 => true, (l1, l2) => new { val1 = l1, val2 = l2 })
+                .Where(a => a.val1 != a.val2);
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.val1} {item.val2}");
+            }
+        }
+
+        public void NonEqualPairs(IEnumerable<int> list1, IEnumerable<int> list2)
+        {
+            var partialResult = list1.Join(list2, l1 => true, l2 => true, (l1, l2) => new { val1 = l1, val2 = l2 }); 
+            var finalResult = partialResult.Where((pair, c) => !partialResult.Take(c)
+                    .Any(combo => (pair.val1 == combo.val2 && pair.val2 == combo.val1)));
+
+            foreach (var item in finalResult)
+            {
+                Console.WriteLine($"{item.val1} {item.val2}");
             }
         }
     }
